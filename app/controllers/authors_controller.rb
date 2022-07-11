b0c87ca591ddeee7c2006fe36020a5b5df0a8e94
marija117 <kuveljic117@gmail.com>
2022-07-11 14:08:11 -0500

    Implement CRUD operations for author.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,22 +1,52 @@
 class AuthorsController < ApplicationController
+    before_action :set_author, only: [:show, :edit, :update, :destroy]
+
     def index
+        @authors = Author.all
     end
   
     def show
     end
   
     def new
+        @author = Author.new
     end
   
     def create
-    end
-  
-    def update
+        @author = Author.new(author_params)
+
+        if @author.save
+            redirect_to @author
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
   
     def edit
     end
+    
+    def update    
+        if @author.update(author_params)
+          redirect_to @author
+        else
+          render :edit, status: :unprocessable_entity
+        end
+    end
   
     def destroy
+        @author.destroy
+    
+        redirect_to root_path, status: :see_other
+    end
+    
+
+    private
+
+    def set_author
+        @author = Author.find(params[:id]) if params[:id]
+    end
+
+    def author_params
+      params.require(:author).permit(:name)
     end
 end
