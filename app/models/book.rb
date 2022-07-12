90710df865190996e50879add96b2c51f12a58f0
marija117 <kuveljic117@gmail.com>
2022-07-12 16:52:18 -0500

    Implement out of stock for books.

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,7 @@ class Book < ApplicationRecord
   belongs_to :author
 
   validates :title, presence: true
+  validates :number_of_hard_copies, presence: true
+
+  scope :out_of_stock, -> { where number_of_hard_copies: [0, nil]}
 end
