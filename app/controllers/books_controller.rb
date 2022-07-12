3d78e0cd1a429b86915aa0a9d61be98c32b267c0
marija117 <kuveljic117@gmail.com>
2022-07-11 19:30:50 -0500

    Implement CRUD operations for book.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
-  
+  before_action :get_author
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
   def index
+    @books = @author.books
   end
 
   def show
@@ -10,14 +13,43 @@ class BooksController < ApplicationController
   end
 
   def create
-  end
+    @book = @author.books.build(book_params)
 
-  def update
+    if @book.save
+      render json: @book, notice: 'Book was successfully created.'
+    else
+      render json: { errors: @book.errors }, status: 422 
+    end
   end
 
   def edit
   end
 
+  def update
+    if @book.update(book_params)
+      render json: @book, notice: 'Book was successfully updated.'
+    else
+      render json: { errors: @book.errors }, status: 422 
+    end
+  end
+
   def destroy
+    @book.destroy
+
+    render json: @author
+  end
+
+  private
+
+  def set_book
+      @book = @author.books.find(params[:id]) if params[:id]
+  end
+
+  def get_author
+      @author = Author.find(params[:author_id]) if params[:author_id]
+  end
+
+  def book_params
+      params.require(:book).permit(:author_id, :title, :number_of_hard_copies)
   end
 end
