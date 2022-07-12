3a8f4e9d02f402767b675f019adccfad5995d1a0
marija117 <kuveljic117@gmail.com>
2022-07-11 19:31:56 -0500

    Add necessary routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "authors#index"
+  
+  devise_for :users
+
+  resources :authors do
+    resources :books
+  end
 end
