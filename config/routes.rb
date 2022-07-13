4e3815746660eb1c8836ec2fad0410399644211c
marija117 <kuveljic117@gmail.com>
2022-07-13 13:42:57 -0500

    Add simple navbar in testing purpose.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  root "devise/sessions#new"
+  root "authors#index"
   
   devise_for :users
 
