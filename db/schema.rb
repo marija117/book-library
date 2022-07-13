ddd9cae9e21f40dfc47746d1817a6d78aa41f99e
marija117 <kuveljic117@gmail.com>
2022-07-13 13:10:16 -0500

    Implement roles using enum.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_234205) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_161241) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -35,17 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_234205) do
     t.index ["user_id"], name: "index_loan_records_on_user_id"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["name"], name: "index_roles_on_name"
-    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,14 +44,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_234205) do
     t.string "full_name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role_id"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
   add_foreign_key "books", "authors"
   add_foreign_key "loan_records", "books"
   add_foreign_key "loan_records", "users"
-  add_foreign_key "users", "roles"
 end
