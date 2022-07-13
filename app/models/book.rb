22e148460d401ead3628fe9b6b0fb6f6866e98a4
marija117 <kuveljic117@gmail.com>
2022-07-12 21:08:22 -0500

    Implement history_of_loaned_books.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,14 @@
 class Book < ApplicationRecord
   include Search
   belongs_to :author
+  has_many :loan_records
 
   validates :title, presence: true
   validates :number_of_hard_copies, presence: true
 
   scope :out_of_stock, -> { where number_of_hard_copies: [0, nil]}
+  scope :history_of_loaned_books, lambda { |user|
+    joins(:loan_records).where("loan_records.user_id": user.id).order("loan_records.created_at": :asc) 
+  }
+
 end
