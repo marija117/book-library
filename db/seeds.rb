000bb61286b41bfca0772ad049f643c9509dfb66
marija117 <kuveljic117@gmail.com>
2022-07-13 16:16:07 -0500

    Fill seeder with data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,114 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!([
+    {
+        full_name: "John Doe",
+        email: "john@doe.com",
+        password: "12345678",
+        password_confirmation: "12345678",
+        role: "librarian"
+    },
+    {
+        full_name: "Joe Smith",
+        email: "joe@amith.com",
+        password: "12345678",
+        password_confirmation: "12345678"
+    },
+    {
+        full_name: "Jane Doe",
+        email: "jane@doe.com",
+        password: "12345678",
+        password_confirmation: "12345678"
+    },
+    {
+        full_name: "Anna Smith",
+        email: "anna@smith.com",
+        password: "12345678",
+        password_confirmation: "12345678"
+    }
+])
+
+Author.create([
+    {
+        name: "Hermann Hesse"
+    },
+    {
+        name: "Kazuo Ishiguro"
+    },
+    {
+        name: "Leo Tolstoy"
+    }
+])
+
+
+Book.create([
+    {
+        title: "Sidarta",
+        number_of_hard_copies: 5,
+        author_id: 1
+    },
+    {
+        title: "Demian",
+        number_of_hard_copies: 5,
+        author_id: 1
+    },
+    {
+        title: "Steppenwolf",
+        number_of_hard_copies: 2,
+        author_id: 1
+    },
+    {
+        title: "Never let me go",
+        number_of_hard_copies: 5,
+        author_id: 2
+    },
+    {
+        title: "Klara and the Sun",
+        number_of_hard_copies: 2,
+        author_id: 2
+    },
+    {
+        title: "War and Peace",
+        number_of_hard_copies: 10,
+        author_id: 3
+    },
+    {
+        title: "Anna Karenina",
+        number_of_hard_copies: 8,
+        author_id: 3
+    },
+    {
+        title: "The Death of Ivan Ilyich",
+        number_of_hard_copies: 3,
+        author_id: 3
+    }
+])
+
+LoanRecord.create([
+    {
+        user_id: "2",
+        book_id: "1"
+    },
+    {
+        user_id: "2",
+        book_id: "3"
+    },
+    {
+        user_id: "2",
+        book_id: "6"
+    },
+    {
+        user_id: "3",
+        book_id: "2"
+    },
+    {
+        user_id: "3",
+        book_id: "3"
+    },
+    {
+        user_id: "2",
+        book_id: "7"
+    }
+])
