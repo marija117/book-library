800c9b07087a87ab99e56aa80e024271c3c0b08a
marija117 <kuveljic117@gmail.com>
2022-07-13 13:10:54 -0500

    implement number of loaned books.

--- app/models/loan_record.rb
+++ app/models/loan_record.rb
@@ -2,5 +2,5 @@ class LoanRecord < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
-  scope :number_of_loaned_books, -> (member_id) { (where user_id: member_id).count}
+  scope :number_of_loaned_books, -> (member_id) { (where user_id: member_id).count < 3}
 end
