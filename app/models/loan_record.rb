3bb5ebdcc5615c9efede9c9f01a45e0811f069b4
marija117 <kuveljic117@gmail.com>
2022-07-13 13:25:39 -0500

    Before saving the loan record subtract one from the number of hard copies.

--- app/models/loan_record.rb
+++ app/models/loan_record.rb
@@ -3,4 +3,10 @@ class LoanRecord < ApplicationRecord
   belongs_to :user
 
   scope :number_of_loaned_books, -> (member_id) { (where user_id: member_id).count < 3}
+  
+  before_save do
+    book = Book.find(self.book_id)
+    book.number_of_hard_copies -= 1
+    book.save!
+  end
 end
