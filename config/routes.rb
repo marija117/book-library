491fe20486106ca942024016027fec2a4fb9d36f
marija117 <kuveljic117@gmail.com>
2022-07-13 13:27:58 -0500

    Modify routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  # root "authors#index"
+  root "devise/sessions#new"
   
   devise_for :users
 
   resources :authors do
     resources :books
   end
+
+  resources :loan_records
 end
